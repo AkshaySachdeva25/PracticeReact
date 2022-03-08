1ffe616d15af6401bceda751f4e7a9bb8ccea3ae
AkshaySachdeva25 <akshay.sachdeva@fareportal.com>
2022-03-08 11:58:57 +0530

    Routes

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Route = ({path, children}) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+    const onLocationChange = () => {
+        setCurrentPath(window.location.pathname)
+    }
+    useEffect(() => {
+        window.addEventListener('popstate',onLocationChange)
+
+        return () => {
+            window.removeEventListener('popstate',onLocationChange)
+        }
+    },[])
+
     return window.location.pathname === path ? children : null;
 }
 
